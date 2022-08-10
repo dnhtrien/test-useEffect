cf11a70770f4be3429fcfab093fb53ea774434ee
dnhtrien <ntrien50@gmail.com>
2022-08-10 13:31:59 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import './component/component.css';
+import { useEffect, useState } from 'react';
+//----------------------------------------------
+//----------------------------------------------
 
-function App() {
+export default function App() {
+  const [color, setColor] = useState('green');
+
+  const [time, setTime] = useState(0);
+  const handleReset = () => {
+    setTime(0);
+    setColor('green');
+  };
+
+  const handleStartTime = () => {
+    setTime(time + 1);
+    setColor('red');
+  };
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     handleStartTime();
+  //   }, 1000);
+  // }, [time]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Vi du UseEffect() Hook</h1>
+      <p>{time}</p>
+      <div className="button">
+        <button type="button" className="inside" style={{ backgroundColor: color, width: '120px', height: ' 30px' }} onClick={handleStartTime}>
+          Start
+        </button>
+      </div>
+      <div className="button">
+        <button type="button" className="inside" style={{ backgroundColor: '#FFE4E1', width: '120px', height: ' 30px' }} onClick={handleReset}>
+          Reset
+        </button>
+      </div>
+      <h1>Cau hoi truyen len</h1>
     </div>
   );
 }
-
-export default App;
